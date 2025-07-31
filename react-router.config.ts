e19464182b0e46f64774ff4d35e056d5b2b209d7
nanasi-1 <test-email>
2025-08-01 00:59:44 +0900

    test

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
 
+console.log("env:", import.meta.env.GITHUB_PAGES, process.env.GITHUB_PAGES)
+
 export default {
   ssr: false,
-  basename: process.env.GITHUB_PAGES ? '/web' : '/'
+  basename: import.meta.env.GITHUB_PAGES ? '/web' : '/'
 } satisfies Config;
