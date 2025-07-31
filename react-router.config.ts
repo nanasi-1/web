3d66e203a8721bad5ac83589637fabc8c70ad92a
nanasi-1 <test-email>
2025-08-01 01:24:50 +0900

    404.htmlを追加

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,8 +1,16 @@
 import type { Config } from "@react-router/dev/config";
+import { copyFile } from 'node:fs/promises';
+import path from 'node:path';
 
 const basename = process.env.GITHUB_PAGES ? '/web/' : '/'
 
 export default {
   ssr: false,
-  basename
+  basename,
+  // https://zenn.dev/ofk/articles/562098fdab1888
+  async buildEnd(args): Promise<void> {
+    if (!args.viteConfig.isProduction) return;
+    const buildPath = args.viteConfig.build.outDir;
+    await copyFile(path.join(buildPath, 'index.html'), path.join(buildPath, '404.html'));
+  },
 } satisfies Config;
